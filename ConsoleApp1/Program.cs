d33e0efa1e152588ccc1c851dba2567196abaac7
MD <arestop783@gmail.com>
2025-11-11 23:17:24 +0300

    Class Human

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -364,3 +364,23 @@ class Program
     }
 }
 
+class Human
+{
+    public string FIO { get; private set; }
+    public int Age { get; private set; }
+    public DateOnly Birthday { get; private set; }
+    public string Gender { get; private set; }
+
+    public Human(string fio, int age, DateOnly birthday, string gender)
+    {
+        FIO = fio;
+        Age = age;
+        Birthday = birthday;
+        Gender = gender;
+    }
+
+    public virtual string GetInfo()
+    {
+        return $"{FIO}, Возраст: {Age}, Пол: {Gender}";
+    }
+}
