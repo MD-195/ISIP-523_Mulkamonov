bb83b71cf771ac57d1d6c47316e8579a3a36e329
MD <arestop783@gmail.com>
2025-11-11 22:53:53 +0300

    Структура проекта

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<Student> students = new List<Student>();
+    static List<Teacher> teachers = new List<Teacher>();
+    static List<Course> courses = new List<Course>();
+    static int nextStudentId = 1;
+    static int nextTeacherId = 1;
+    static int nextCourseId = 1;
+
+    
